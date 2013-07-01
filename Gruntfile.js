6325602cf7fc2a07d419077e34b6571c7c07db0d
Jonas Pommerening <jpommerening@mbp.fritz.box>
2013-07-01 23:51:37 +0200

    Add clean task

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,7 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    clean: ['dist/'],
     requirejs: {
       dist: {
         options: {
@@ -34,10 +35,10 @@ module.exports = function(grunt) {
     }
   });
 
-  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-requirejs');
 
   // Default task(s).
-  grunt.registerTask('default', ['requirejs']);
+  grunt.registerTask('default', ['clean', 'requirejs']);
 
 };
